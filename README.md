e6d4c82b1be946958f33b8de23939c413ab5402f
Nik L <nik.lilovski@outlook.com>
2024-10-07 18:49:32 +0100

    Added typing annotations

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # pyBuildReleaseTest
 
 Python port of [rBuildReleaseTest](https://github.com/nik01010/rBuildReleaseTest)
+
+## Local usage
+### Local Build
+pip install -q build
+python -m build
+
+## Local Install
+pip install .\dist\pybuildreleasetest-0.1.0.tar.gz
--- src/pyBuildReleaseTest/DataAccess_ApplicationDbContext.py
+++ src/pyBuildReleaseTest/DataAccess_ApplicationDbContext.py
@@ -1,31 +1,36 @@
 import pyodbc
 
 class ApplicationDbContext:
-    def __init__(self, connection_driver = "ODBC Driver 17 for SQL Server", connection_server = "localhost", connection_database = "master"):
+    def __init__(
+        self, 
+        connection_driver: str = "ODBC Driver 17 for SQL Server", 
+        connection_server: str = "localhost", 
+        connection_database: str = "master"
+    ):
         # TODO: add validations
         # TODO: add logging
-        self._connection_driver = connection_driver
-        self._connection_server = connection_server
-        self._connection_database = connection_database
+        self._connection_driver: str = connection_driver
+        self._connection_server: str = connection_server
+        self._connection_database: str = connection_database
 
-        self._connection_string = self._get_connection_string()
+        self._connection_string: str = self._get_connection_string()
 
-        self.database_connection = self._create_database_connection()
+        self.database_connection: pyodbc.Connection = self._create_database_connection()
     
     def _get_connection_string(self):
         # TODO: add logging
-        connection_string = f'DRIVER={self._connection_driver};SERVER={self._connection_server};DATABASE={self._connection_database};Trusted_Connection=yes;'
+        connection_string: str = f'DRIVER={self._connection_driver};SERVER={self._connection_server};DATABASE={self._connection_database};Trusted_Connection=yes;'
         print(connection_string)
 
         return(connection_string)
     
-    def _create_database_connection(self):
+    def _create_database_connection(self) -> pyodbc.Connection:
         # TODO: add logging
         # TODO: add error handling
-        database_connection = pyodbc.connect(self._connection_string)
+        database_connection: pyodbc.Connection = pyodbc.connect(self._connection_string)
         
         return(database_connection)
 
-    def close_database_connection(self):
+    def close_database_connection(self) -> None:
         # TODO: add logging
         self.database_connection.close()
