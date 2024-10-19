be78048e318999f117029e9498dcfcec4b0c25cc
Nik L <nik.lilovski@outlook.com>
2024-10-19 14:47:38 +0100

    Tweaked person class test

--- tests/test-DataModel_Person.py
+++ tests/test-DataModel_Person.py
@@ -6,6 +6,8 @@ class TestDataModelPerson(unittest.TestCase):
     def test_create_new_person_should_return_correct_class(self):
         # Arrange / Act
         expected_id: int = 123
+        expected_last_name: str = "Doe"
+        expected_modified_date: datetime = datetime.now()
         test_person: Person = Person(
             BusinessEntityID = expected_id,
             PersonType = "AB",
@@ -13,18 +15,20 @@ class TestDataModelPerson(unittest.TestCase):
             Title = "Mr",
             FirstName = "John",
             MiddleName = None,
-            LastName = "Doe",
+            LastName = expected_last_name,
             Suffix = None,
             EmailPromotion = 1,
             AdditionalContactInfo = None,
             Demographics = None,
             rowguid = "abc",
-            ModifiedDate = datetime.now()
+            ModifiedDate = expected_modified_date
         )
 
         # Assert
         self.assertIsInstance(test_person, Person)
         self.assertEqual(expected_id, test_person.BusinessEntityID)
+        self.assertEqual(expected_last_name, test_person.LastName)
+        self.assertEqual(expected_modified_date, test_person.ModifiedDate)
 
 if __name__ == '__main__':
     unittest.main()
